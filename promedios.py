dee4f7be4a3afe923fef9a0162937ce238b5ce42
Moni-ib <monica.ibarra@iteso.mx>
2023-11-05 14:54:07 -0600

    prueba dos modificando el append

--- promedios.py
+++ promedios.py
@@ -9,16 +9,16 @@ with open('data/calificaciones.txt', 'r') as alumnos:
         calificaciones = [float(calificacion) for calificacion in individual[2:]]
         promedio = sum(calificaciones) / len(calificaciones)
         promedio_redondeado = round(promedio,1)
-        promedios.append((nombre, apellido, promedio_redondeado))
+        promedios.append([nombre, apellido_may, promedio_redondeado])
 
 #'data/promedios.txt' es el nombre del archivo de salida,
-# y 'w' es el modo en el que se abre que es escritura.
+# Abrir el archivo de promedios en modo agregar ('a')
 with open('data/promedios.txt', 'w') as final:
     for promedio in promedios:
         nombre = promedio[0]
         apellido = promedio[1]
         promedio_final = promedio[2]
 #Se escribe una línea en el archivo promeidos con el formato "Apellido, Nombre: Promedio"
-    final.write(f"{apellido}, {nombre}: {promedio_final:.1f}\n")
+        final.write(f"{apellido}, {nombre}: {promedio_final:.1f}\n")
 
 
