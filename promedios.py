e9fb52c415e17351f11c5b107d2b68b0c7406268
Moni-ib <monica.ibarra@iteso.mx>
2023-11-07 18:23:29 -0600

    reencriptar

--- promedios.py
+++ promedios.py
@@ -1,3 +1,9 @@
+"""
+Mónica Ibarra Herrera
+10 de noviembre de 2023
+Programa que con el archvio de calificaciones crea un archivo de texto para 
+generar el promedio de los alumnos en un formato en específico
+"""
 #Se abre el archivo en modo lectura
 with open('data/calificaciones.txt', 'r') as alumnos:
     promedios = []
@@ -12,13 +18,13 @@ with open('data/calificaciones.txt', 'r') as alumnos:
         promedios.append([nombre, apellido_may, promedio_redondeado])
 
 #'data/promedios.txt' es el nombre del archivo de salida,
-# Abrir el archivo de promedios en modo agregar ('a')
+# Abrir el archivo de promedios en modo escritura ('w')
 with open('data/promedios.txt', 'w') as final:
     for promedio in promedios:
         nombre = promedio[0]
         apellido = promedio[1]
         promedio_final = promedio[2]
-#Se escribe una línea en el archivo promeidos con el formato "Apellido, Nombre: Promedio"
+#Se escribe una línea en el archivo promedios con el formato "Apellido, Nombre: Promedio"
         final.write(f"{apellido}, {nombre}: {promedio_final:.1f}\n")
 
 
---
+++ reencriptar-ejemplo.py
@@ -0,0 +1,3 @@
+from reencriptar import *
+
+reencriptar_archivo("data/Asimov, Isaac - Cómo ocurrió.txt", -1)
